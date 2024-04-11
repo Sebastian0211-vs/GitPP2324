7642b8be12b9cf6362763b8f88f6e03c8514a142
louis272 <louis.schanen@gmail.com>
2024-04-11 21:29:51 +0200

    Brouillon du concept d'updateur

--- UpdateurDeFichier/Updater.py
+++ UpdateurDeFichier/Updater.py
@@ -10,4 +10,5 @@ def writeFile(chemin_fichier, chemin_fichier_tmp):
     with open(chemin_fichier_tmp, "w") as fichier_tmp, open(chemin_fichier, "r") as fichier, open(chemin_fichier_ajout, "r") as fichier_ajout:
         for ligne in fichier:
             fichier_tmp.write(ligne)
-
+        for ligne in fichier_ajout:
+            fichier_tmp.write(ligne)
