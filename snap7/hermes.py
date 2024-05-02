a27bcd1da5f637a22bc980ff96f1cd0198817a90
louis272 <louis.schanen@gmail.com>
2024-05-02 13:55:22 +0200

    Adapt watchdog to snap7 protocol

---
+++ snap7/hermes.py
@@ -0,0 +1,137 @@
+#-*- coding: utf-8 -*-
+#from asyncua import Client, ua
+from flask import Flask, jsonify
+import CommunicationAPIS7 as commS7
+import ip_addresses as ip
+import requests
+import snap7
+
+CONNECTED_PLC = None
+
+var_api_out = ["Mw_API_CVSortieStandard","Mw_API_CVSortieSecours","Mw_API_CVEntree"]
+
+var_api_in = ["Mw_master_nb_billes_sortie_normal","Mw_master_nb_billes_sortie_secours","Mw_master_nb_billes_entree"]
+
+app = Flask(__name__)
+
+@app.route('/API_IP/<string:ip>', methods=['GET'])
+def API_IP(ip):
+    global CONNECTED_PLC 
+    try:
+        plc = snap7.client.Client()
+        plc.connect(ip, 0, 1)  # IP address, rack, slot
+        CONNECTED_PLC = plc
+        return jsonify(message=f"API IP: {ip}"), 200
+    except Exception as e:
+        return jsonify(error=str(e)), 500
+
+# Ensemble des modes possibles
+possibles_modes = {"mono", "multi"}
+@app.route('/check_possible', methods=['GET'])
+def check_possible():
+    global CONNECTED_PLC
+    try:
+        # TODO Check if it works
+        if (not CONNECTED_PLC.get_connected()):
+            return jsonify(message=f"API non connecté"), 300
+        return jsonify(message=f"API connecté"), 200
+    except Exception as e:
+        return jsonify(error=str(e)), 500
+
+@app.route('/multi/<string:BOOL>', methods=['GET'])
+def multi(BOOL):
+    global CONNECTED_PLC
+    try:
+        if BOOL == "True":
+            commS7.writeMemory(CONNECTED_PLC, "Mx_master_multi_possible", True)
+        else:
+            commS7.writeMemory(CONNECTED_PLC, "Mx_master_multi_possible", False)
+
+        return jsonify(message=f"Mode multi activé"), 200
+    except Exception as e:
+        return jsonify(error=str(e)), 500
+
+
+
+@app.route('/sortie/<string:request>', methods=['GET'])
+def demande_multi(request):
+    global CONNECTED_PLC
+    try:
+        commS7.writeMemory(CONNECTED_PLC, request, True)
+    except Exception as e :
+        return jsonify(error=str(e)), 500
+    
+
+@app.route('/trigger/<string:request>', methods=['GET'])
+def trigger(request):
+    global CONNECTED_PLC
+    try:
+        valeur = commS7.readMemory(CONNECTED_PLC, request)
+        if valeur:
+            return jsonify(message=f"{valeur}"), 200
+        else :
+            return jsonify(message=f"{valeur}"), 300
+    except Exception as e:
+        return jsonify(error=str(e)), 500
+
+@app.route('/multinbr/<string:BOOL>', methods=['GET'])
+def multinbr(BOOL):
+    global CONNECTED_PLC
+    try:
+        if BOOL == "True":
+            commS7.writeMemory(CONNECTED_PLC, "Mx_master_multi+de3", True)
+        else:
+            commS7.writeMemory(CONNECTED_PLC, "Mx_master_multi+de3", False)
+
+        return jsonify(message=f"Mode multi activé"), 200
+    except Exception as e:
+        return jsonify(error=str(e)), 500
+
+
+
+@app.route('/compteur_bille', methods=['GET'])
+def compteur_bille():
+    global CONNECTED_PLC
+    storage = []
+    for name in var_api_out:
+        try:
+            storage.append(commS7.readMemory(CONNECTED_PLC, name))
+        except Exception as e:
+            return jsonify(error=str(e)), 500
+    return jsonify(storage), 200
+
+
+@app.route('/compteur_bille/<int:val>', methods=['GET'])
+def set_compteur_bille(val):
+    global CONNECTED_PLC
+    try:
+        for name in var_api_in:
+            commS7.writeMemory(CONNECTED_PLC, name, val)
+        return jsonify(message=f"Valeur de compteur bille modifiée"), 200
+    except Exception as e:
+        return jsonify(error=str(e)), 500
+
+
+@app.route('/deconnection', methods=['GET'])
+def deconnection():
+    global CONNECTED_PLC
+    try:
+        CONNECTED_PLC.destroy()
+        CONNECTED_PLC = None
+        return jsonify(message=f"API déconnecté"), 200
+    except Exception as e:
+        return jsonify(error=str(e)), 500
+
+# Gestionnaires d'erreur
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify(error="Non trouve"), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return jsonify(error="Erreur interne du serveur"), 500
+
+
+# Point d'entrée principal pour exécuter l'application
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
---
+++ snap7/watchdog.py
@@ -0,0 +1,161 @@
+from flask import Flask, jsonify
+from apscheduler.schedulers.background import BackgroundScheduler
+import time
+import ip_addresses as ip
+import requests
+import snap7
+import logging
+import numpy as np
+import time
+
+app = Flask(__name__)
+logging.basicConfig(filename='apps.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+print("""\
+
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡟⠋⠈⠙⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠤⢤⡀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠈⢇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠞⠀⠀⢠⡜⣦⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡃⠀⠀⠀⠀⠈⢷⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠊⣠⠀⠀⠀⠀⢻⡘⡇
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠃⠀⠀⠀⠀⠀⠀⠙⢶⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡠⠚⢀⡼⠃⠀⠀⠀⠀⠸⣇⢳
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⠀⣀⠖⠀⠀⠀⠀⠉⠀⠀⠈⠉⠛⠛⡛⢛⠛⢳⡶⠖⠋⠀⢠⡞⠀⠀⠀⠐⠆⠀⠀⣿⢸
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣦⣀⣴⡟⠀⠀⢶⣶⣾⡿⠀⠀⣿⢸
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀⡠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣏⠀⠀⠀⣶⣿⣿⡇⠀⠀⢏⡞
+⠀⠀⠀⠀⠀⠀⢀⡴⠛⠀⠀⠀⠀⠀⠀⠀⠀⢀⢀⡾⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢦⣤⣾⣿⣿⠋⠀⠀⡀⣾⠁
+⠀⠀⠀⠀⠀⣠⠟⠁⠀⠀⠀⣀⠀⠀⠀⠀⢀⡟⠈⢀⣤⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⣏⡁⠀⠐⠚⠃⣿⠀
+⠀⠀⠀⠀⣴⠋⠀⠀⠀⡴⣿⣿⡟⣷⠀⠀⠊⠀⠴⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠀⠀⠀⠀⢹⡆
+⠀⠀⠀⣴⠃⠀⠀⠀⠀⣇⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⡶⢶⣶⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇
+⠀⠀⣸⠃⠀⠀⠀⢠⠀⠊⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀⢲⣾⣿⡏⣾⣿⣿⣿⣿⠖⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢧
+⠀⢠⡇⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠈⠛⠿⣽⣿⡿⠏⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡜
+⢀⡿⠀⠀⠀⠀⢀⣤⣶⣟⣶⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇
+⢸⠇⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇
+⣼⠀⢀⡀⠀⠀⢷⣿⣿⣿⣿⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡇
+⡇⠀⠈⠀⠀⠀⣬⠻⣿⣿⣿⡿⠙⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠁
+⢹⡀⠀⠀⠀⠈⣿⣶⣿⣿⣝⡛⢳⠭⠍⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠃⠀
+⠸⡇⠀⠀⠀⠀⠙⣿⣿⣿⣿⣿⣿⣷⣦⣀⣀⣀⣤⣤⣴⡶⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠇⠀⠀
+⠀⢿⡄⠀⠀⠀⠀⠀⠙⣇⠉⠉⠙⠛⠻⠟⠛⠛⠉⠙⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡰⠋⠀⠀⠀
+⠀⠈⢧⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠞⠁⠀⠀⠀⠀
+⠀⠀⠘⢷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠞⠁⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠱⢆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡴⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠛⢦⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⠴⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠲⠤⣤⣤⣤⣄⠀⠀⠀⠀⠀⠀⠀⢠⣤⣤⠤⠴⠒⠛⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+
+ """)
+def job():
+    logging.info('Job started')
+    NombreAPIConnceted = 0
+    time.sleep(1)
+    multi_possible(NombreAPIConnceted)
+    nombremultisql = 0
+    checkplusde3(nombremultisql)
+    get_info()
+    compteur_bille()
+    logging.info('Job finished')
+    
+valid_request = ["Mx_master_demande_multi" , "Mx_master_alarme_urgence", "Mx_master_probleme_obstruation", "Mx_master_running", "Mx_master_arret_auto","Mx_master_quittance_obstruation","Mx_master_quittance_alarme","Mx_master_alerte_billes_perdus","Mx_master_alerte_sortie_secours"]
+
+def compteur_bille():
+    listevide = [0,0,0]
+    A = np.array(listevide)
+    for ip in ip.ip_addresses.values():
+        try:
+            response = requests.get(f"http://{ip['RASP_catch']}:8000/compteur_bille")
+            A += response
+            print(response.json())
+        except Exception as e:
+            logging.error(e)
+    
+    try:
+        for ip in ip.ip_addresses.values():
+            for i in range(3):
+                requests.get(f"http://{ip['RASP_catch']}:8000/compteur_bille/{A[i]}/")
+    except Exception as e:
+        logging.error(e)
+           
+
+def get_info():
+    for request in valid_request:
+        for ip in ip.ip_addresses.values():
+            try:
+                response = requests.get(f"http://{ip['RASP_catch']}:8000/trigger/{request}")
+                if response.status_code == 200:
+                    logging.info(response.json())
+                    for ip in ip.ip_addresses.values():
+                        try:
+                            response = requests.get(f"http://{ip['RASP_catch']}:8000/sortie/{request}")
+                            logging.info(response.json())
+                        except Exception as e:
+                            logging.error(e)
+                    break
+                    
+            except Exception as e:
+                logging.error(e)
+
+def multi_possible(NombreAPIConnceted):
+    for ip in ip.ip_addresses.values():
+        try:
+            checker = requests.get(f"http://{ip['RASP_catch']}:8000/check_possible")
+            if checker.status_code == 200:
+                NombreAPIConnceted += 1
+        except Exception as e:
+            logging.error(e)
+
+    if NombreAPIConnceted > 1:
+        print("Mode multi disponible")
+        for ip in ip.ip_addresses.values():
+            try:
+                requests.get(f"http://{ip['RASP_catch']}:8000/multi/True")
+            except Exception as e:
+                logging.error(e)
+    else:
+        print("Mode multi indisponible")
+        for ip in ip.ip_addresses.values():
+            try:
+                requests.get(f"http://{ip['RASP_catch']}:8000/multi/False")
+            except Exception as e:
+                logging.error(e)
+
+def checkplusde3(nombremultisql):
+    if nombremultisql >=3:
+        print("Multi sur plus de 3 châssis")
+        for ip in ip.ip_addresses.values():
+            try:
+                requests.get(f"http://{ip['RASP_catch']}:8000/multinbr/True")
+            except Exception as e:
+                logging.error(e)
+    else:
+        print("Multi sur moins de 3 châssis")
+        for ip in ip.ip_addresses.values():
+            try:
+                requests.get(f"http://{ip['RASP_catch']}:8000/multinbr/False")
+            except Exception as e:
+                logging.error(e)
+
+####################################################################################
+
+
+
+
+
+
+
+#####################################################################################
+
+for ip in ip.ip_addresses.values():
+    url = f"http://{ip['RASP_catch']}:8000/API_IP/{ip['API']}"
+    try:
+        response = requests.get(url)
+        print(response.json())
+    except Exception as e:
+        logging.error(e)
+
+scheduler = BackgroundScheduler(daemon=True)
+scheduler.add_job(job, 'interval', seconds=10)
+scheduler.start()
+
+
+# Point d'entrée principal pour exécuter l'application
+if __name__ == '__main__':
+    logging.info('Application started')
+    app.run(host='0.0.0.0', port=8000)
+    logging.info('Application finished')
+
